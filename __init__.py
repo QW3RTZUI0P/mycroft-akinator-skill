24c9dbd826b501e3554fbbd5ca26e7e2fa90841b
QW3RTZUI0P <51383519+QW3RTZUI0P@users.noreply.github.com>
2021-09-05 11:07:03 +0200

    Basic first akinator game, but not yet functional

--- __init__.py
+++ __init__.py
@@ -1,13 +1,18 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import akinator
 
 class MycroftAkinator(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('akinator.mycroft.intent')
+    @intent_file_handler('start.akinator.intent')
     def handle_akinator_mycroft(self, message):
-        self.speak_dialog('akinator.mycroft')
+        aki = akinator.Akinator()
+        q = aki.start_game()
+        while aki.progression <= 80:
+            a = self.ask_yesno(q)
+            q = aki.answer(a)
+        aki.win()
 
 
 def create_skill():
