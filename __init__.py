076dd9da11df87abc17772c213fb9e9efc67b928
QW3RTZUI0P <51383519+QW3RTZUI0P@users.noreply.github.com>
2021-09-11 16:29:37 +0200

    Corrected a small mistake in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,25 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.api import DeviceApi
 import akinator
 
 class MycroftAkinator(MycroftSkill):
+
+    language_code = ""
+
     def __init__(self):
         MycroftSkill.__init__(self)
+
+    def initialize(self):
+        MycroftAkinator.language_code = str(DeviceApi().get_location()["city"]["state"]["country"]["code"]).lower()
 
     @intent_file_handler('start.akinator.intent')
     def handle_akinator_mycroft(self, message):
         aki = akinator.Akinator()
-        q = aki.start_game()
-        while aki.progression <= 80:
-            a = self.ask_yesno(q)
-            q = aki.answer(a)
-        aki.win()
+        question = aki.start_game(language = "en")
+        while aki.progression <= 85:
+            answer = self.ask_yesno(question)
+            question = aki.answer(answer)
+        self.speak(aki.win())
 
 
 def create_skill():
